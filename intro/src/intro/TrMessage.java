92e4b17cb894b83fbf4719c0ebbb23fef2a33544
ege deniz <ege_denzo_99@hotmail.com>
2021-04-24 18:21:49 +0300

    Update

--- intro/src/intro/TrMessage.java
+++ intro/src/intro/TrMessage.java
@@ -4,7 +4,7 @@ public class TrMessage implements HelloWord {
 
 	@Override
 	public void getMessage() {
-		System.out.println("Merhaba Kardeþ Sisteme Hoþgeldin.");
+		System.out.println("Merhaba Sisteme Hoþgeldiniz.");
 		
 	}
 
